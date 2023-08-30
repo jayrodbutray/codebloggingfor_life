130d855f23cdf097d08a84aa369359104a1646b1
JayRodButray <jayrodbutray@gmail.com>
2023-08-29 17:52:48 -0700

    add some css

--- public/js/login.js
+++ public/js/login.js
@@ -44,9 +44,8 @@ const loginFormHandler = async (event) => {
     
     
     
-    document
-      .querySelector('.login-form')
-      .addEventListener('submit', loginFormHandler);
+    const loginForm = document.querySelector('.login-form');
+     loginForm.addEventListener('submit', loginFormHandler);
     
     document
       .querySelector('.signup-form')
